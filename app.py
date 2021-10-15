016e41fb39bbed43e099f7b63d3121cd6626dc3a
Zoltan Pechinger <zoltan.pechinger@dxc.com>
2021-10-15 11:14:00 +0200

    added a different page for each behavior

--- app.py
+++ app.py
@@ -2,11 +2,25 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():  # put application's code here
     return render_template("index.html")
 
+@app.route("/time")
+def time():
+    return render_template("time.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.html")
+
+@app.route("/addtocart")
+def add_to_cart():
+    return render_template("addtocart.html")
+
+@app.route('/payment')
+def payment():
+    return render_template("payment.html")
 
 if __name__ == '__main__':
     app.run()
