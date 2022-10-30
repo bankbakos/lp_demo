6c75a2a289f32033c48d35809fab3c95b871c25f
mrestum2 <musztafa.restum2@dxc.com>
2022-10-30 17:00:49 +0100

    up

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def mvm():
 
 @app.route('/mvmnext')
 def mvmnext():
-    return render_template("MVMNext2.html")
+    return render_template("MVMNext.html")
     
 @app.route('/vfh_virtus')
 def vfh_virtus():
